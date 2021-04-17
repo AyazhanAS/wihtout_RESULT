def76663c96c13819457965bd64544446c7f6ab5
Ayazhan <ayazhan.shansharova@mail.ru>
2021-04-17 15:44:58 +0600

    comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__container">
+        <div className="not__found">
+          <p>По запросу "Example" ничего не найдено!</p>
+        </div>
+        <div className="search__input__block">
+          <input
+            placeholder="Введите запрос"
+            type="text"
+            className="search__input"
+          />
+        </div>
+
+      </div>
+
     </div>
   );
 }
